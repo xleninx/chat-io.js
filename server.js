38a027384c77bbe739e4c260bc6140101d5c6e6a
Lenin Luque <xleninx@gmail.com>
2015-09-04 22:14:49 -0300

    Add comments to server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use stric' // this is for make code clean with ES6
 
+// Constants
 const http = require('http')
 const port = process.env.PORT || 8080
-
 const server = http.createServer(onRequest)
 
+// EventEmitter
 server.on('request', onRequest)
 server.on('listening', onListening)
 
+// Call listen to server
 server.listen(port)
 
 // Functions
