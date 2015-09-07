6c6479fbf8032d2fe4eeb6f9c67442de1fc9abb7
Lenin Luque <xleninx@gmail.com>
2015-09-07 18:34:11 -0300

    Allow record video in client side

--- public/app.js
+++ public/app.js
@@ -1,7 +1,486 @@
-const record = document.querySelector('#record')
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict'
+const Webrtc2Images = require('webrtc2images')
 
+const rtc = new Webrtc2Images({
+  width: 200,
+  height: 200,
+  frames: 10,
+  type: 'image/jpeg',
+  quality: 0.4,
+  interval: 200
+})
+
+rtc.startVideo(function(err){
+  if (err) return logError(err)
+})
+
+const record = document.querySelector('#record')
 record.addEventListener('click', function(e){
   e.preventDefault()
 
-  console.log('Button clicked')
-})
+  rtc.recordVideo(function(err, frames){
+    if (err) return logError(err)
+    console.log(frames)
+  })
+}, false)
+
+function logError(err){
+  console.log(err)
+}
+
+},{"webrtc2images":2}],2:[function(require,module,exports){
+var Streamer = require('./lib/streamer');
+var Recorder = require('./lib/recorder');
+
+module.exports = function (config) {
+  'use strict';
+  document.createElement('canvas');
+  var streamer = new Streamer(config);
+  var recorder = new Recorder(config);
+
+  var preview = document.getElementById('video-preview');
+
+  this.startVideo = function (callback) {
+    streamer.startVideo(function (err, data) {
+      if (err) {
+        callback(err);
+      } else {
+        streamer.video = data.videoElement;
+        streamer.video.width = data.videoElement.width;
+        streamer.video.height = data.videoElement.height;
+        preview.appendChild(streamer.video);
+        streamer.video.play();
+        callback();
+      }
+    });
+  };
+
+  this.recordVideo = function (callback) {
+    recorder.video = streamer.video;
+    recorder.getScreenshot(function () {
+      callback(null, recorder.videoFrames);
+    });
+  };
+
+  this.changeCamera = function (callback) {
+    preview.innerHTML = '';
+    streamer.stopVideo();
+    recorder.video = null;
+    this.startVideo(callback);
+  };
+};
+
+},{"./lib/recorder":4,"./lib/streamer":5}],3:[function(require,module,exports){
+module.exports = function(msg) {
+  var debugMsg = document.getElementById('debug-msg');
+  if (debugMsg) {
+    debugMsg.textContent = msg;
+  }
+};
+
+},{}],4:[function(require,module,exports){
+var debug = require('./debug');
+var defaults = require('lodash.defaults');
+
+module.exports = function (options) {
+  'use strict';
+  this.videoFrames = [];
+
+  defaults(options, {
+    type: 'image/jpeg',
+    frames: 10,
+    interval: 200,
+    quality: 0.4,
+  });
+
+  // This is where we change the time lapse count
+  var interval = options.interval;
+  var pendingFrames = options.frames;
+  var type = options.type;
+  var quality = options.quality;
+
+  var canvas = document.createElement('canvas');
+  var img = document.createElement('img');
+  var self = this;
+
+  var captureFrame = function (pendingFrames, callback) {
+    canvas.getContext('2d')
+    .drawImage(self.video, 0, 0, canvas.width, canvas.height);
+
+    img.src = canvas.toDataURL(type, quality);
+
+    var saveFrame = function () {
+      pendingFrames--;
+      debug(pendingFrames);
+      self.videoFrames.push(img.src);
+      captureFrame(pendingFrames, callback);
+    };
+
+    if (pendingFrames > 0) {
+      setTimeout(saveFrame, interval);
+      return;
+    }
+
+    debug('');
+    callback(true);
+    self.videoFrames = [];
+  };
+
+  this.getScreenshot = function (callback) {
+    this.videoFrames = [];
+    if (this.video) {
+      canvas.width = this.video.width;
+      canvas.height = this.video.height;
+      captureFrame(pendingFrames, callback);
+    }
+  };
+};
+
+},{"./debug":3,"lodash.defaults":6}],5:[function(require,module,exports){
+var defaults = require('lodash.defaults');
+
+var setUrl = function () {
+  window.url = window.URL || window.webkitURL || window.mozURL || window.msURL;
+};
+
+var setNav = function() {
+  navigator.getMedia = navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia;
+};
+
+module.exports = function (options) {
+  'use strict';
+
+  defaults(options, {
+    width: 320,
+    height: 180
+  });
+  var width = options.width;
+  var height = options.height;
+
+  var self = this;
+
+  setUrl();
+  setNav();
+
+  function streamMedia(callback) {
+    navigator.getMedia({
+      audio: false,
+      video: {
+        optional: [
+          { minHeight: height },
+          { maxHeight: height },
+          { minWidth: width },
+          { maxWidth: width }
+        ]
+      }
+    }, function (stream) {
+      if (self.videoElement.mozSrcObject) {
+        self.videoElement.mozSrcObject = stream;
+      } else {
+        self.videoElement.src = window.url.createObjectURL(stream);
+      }
+
+      self.videoElement.play();
+      callback(null, stream);
+    }, function (err) {
+      callback(err);
+    });
+  }
+
+  /**
+  * Requests permission for using the user's camera,
+  * starts reading video from the selected camera.
+  */
+  function startStreaming(callback) {
+    var attempts = 0;
+    self.videoElement = document.createElement('video');
+    self.videoElement.autoplay = true;
+    var streaming = false;
+
+    self.videoElement.addEventListener('canplay', function (ev) {
+      if (!streaming) {
+        self.videoElement.setAttribute('width', width);
+        self.videoElement.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    streamMedia(callback);
+  }
+
+  /**
+  * Try to initiate video streaming.
+  */
+  this.startVideo = function (callback) {
+    if (navigator.getMedia) {
+      startStreaming(function (err, stream) {
+        if (err) {
+          callback(err);
+        } else {
+
+          // Keep references, for stopping the stream later on.
+          self.cameraStream = stream;
+          self.video = self.videoElement;
+
+          callback(null, {
+            stream: self.stream,
+            videoElement: self.video
+          });
+        }
+      });
+    } else {
+      callback(new Error('Could not stream video'));
+    }
+  };
+
+  this.stopVideo = function () {
+    if (this.cameraStream) {
+      this.cameraStream.stop();
+    }
+
+    if (this.video) {
+      this.video.pause();
+      this.video.src = null;
+      this.video = null;
+    }
+  };
+};
+
+},{"lodash.defaults":6}],6:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+var keys = require('lodash.keys'),
+    objectTypes = require('lodash._objecttypes');
+
+/**
+ * Assigns own enumerable properties of source object(s) to the destination
+ * object for all destination properties that resolve to `undefined`. Once a
+ * property is set, additional defaults of the same property will be ignored.
+ *
+ * @static
+ * @memberOf _
+ * @type Function
+ * @category Objects
+ * @param {Object} object The destination object.
+ * @param {...Object} [source] The source objects.
+ * @param- {Object} [guard] Allows working with `_.reduce` without using its
+ *  `key` and `object` arguments as sources.
+ * @returns {Object} Returns the destination object.
+ * @example
+ *
+ * var object = { 'name': 'barney' };
+ * _.defaults(object, { 'name': 'fred', 'employer': 'slate' });
+ * // => { 'name': 'barney', 'employer': 'slate' }
+ */
+var defaults = function(object, source, guard) {
+  var index, iterable = object, result = iterable;
+  if (!iterable) return result;
+  var args = arguments,
+      argsIndex = 0,
+      argsLength = typeof guard == 'number' ? 2 : args.length;
+  while (++argsIndex < argsLength) {
+    iterable = args[argsIndex];
+    if (iterable && objectTypes[typeof iterable]) {
+    var ownIndex = -1,
+        ownProps = objectTypes[typeof iterable] && keys(iterable),
+        length = ownProps ? ownProps.length : 0;
+
+    while (++ownIndex < length) {
+      index = ownProps[ownIndex];
+      if (typeof result[index] == 'undefined') result[index] = iterable[index];
+    }
+    }
+  }
+  return result
+};
+
+module.exports = defaults;
+
+},{"lodash._objecttypes":7,"lodash.keys":8}],7:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+
+/** Used to determine if values are of the language type Object */
+var objectTypes = {
+  'boolean': false,
+  'function': true,
+  'object': true,
+  'number': false,
+  'string': false,
+  'undefined': false
+};
+
+module.exports = objectTypes;
+
+},{}],8:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+var isNative = require('lodash._isnative'),
+    isObject = require('lodash.isobject'),
+    shimKeys = require('lodash._shimkeys');
+
+/* Native method shortcuts for methods with the same name as other `lodash` methods */
+var nativeKeys = isNative(nativeKeys = Object.keys) && nativeKeys;
+
+/**
+ * Creates an array composed of the own enumerable property names of an object.
+ *
+ * @static
+ * @memberOf _
+ * @category Objects
+ * @param {Object} object The object to inspect.
+ * @returns {Array} Returns an array of property names.
+ * @example
+ *
+ * _.keys({ 'one': 1, 'two': 2, 'three': 3 });
+ * // => ['one', 'two', 'three'] (property order is not guaranteed across environments)
+ */
+var keys = !nativeKeys ? shimKeys : function(object) {
+  if (!isObject(object)) {
+    return [];
+  }
+  return nativeKeys(object);
+};
+
+module.exports = keys;
+
+},{"lodash._isnative":9,"lodash._shimkeys":10,"lodash.isobject":11}],9:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+
+/** Used for native method references */
+var objectProto = Object.prototype;
+
+/** Used to resolve the internal [[Class]] of values */
+var toString = objectProto.toString;
+
+/** Used to detect if a method is native */
+var reNative = RegExp('^' +
+  String(toString)
+    .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    .replace(/toString| for [^\]]+/g, '.*?') + '$'
+);
+
+/**
+ * Checks if `value` is a native function.
+ *
+ * @private
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if the `value` is a native function, else `false`.
+ */
+function isNative(value) {
+  return typeof value == 'function' && reNative.test(value);
+}
+
+module.exports = isNative;
+
+},{}],10:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+var objectTypes = require('lodash._objecttypes');
+
+/** Used for native method references */
+var objectProto = Object.prototype;
+
+/** Native method shortcuts */
+var hasOwnProperty = objectProto.hasOwnProperty;
+
+/**
+ * A fallback implementation of `Object.keys` which produces an array of the
+ * given object's own enumerable property names.
+ *
+ * @private
+ * @type Function
+ * @param {Object} object The object to inspect.
+ * @returns {Array} Returns an array of property names.
+ */
+var shimKeys = function(object) {
+  var index, iterable = object, result = [];
+  if (!iterable) return result;
+  if (!(objectTypes[typeof object])) return result;
+    for (index in iterable) {
+      if (hasOwnProperty.call(iterable, index)) {
+        result.push(index);
+      }
+    }
+  return result
+};
+
+module.exports = shimKeys;
+
+},{"lodash._objecttypes":7}],11:[function(require,module,exports){
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="npm" -o ./npm/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+var objectTypes = require('lodash._objecttypes');
+
+/**
+ * Checks if `value` is the language type of Object.
+ * (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
+ *
+ * @static
+ * @memberOf _
+ * @category Objects
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if the `value` is an object, else `false`.
+ * @example
+ *
+ * _.isObject({});
+ * // => true
+ *
+ * _.isObject([1, 2, 3]);
+ * // => true
+ *
+ * _.isObject(1);
+ * // => false
+ */
+function isObject(value) {
+  // check if the value is the ECMAScript language type of Object
+  // http://es5.github.io/#x8
+  // and avoid a V8 bug
+  // http://code.google.com/p/v8/issues/detail?id=2291
+  return !!(value && objectTypes[typeof value]);
+}
+
+module.exports = isObject;
+
+},{"lodash._objecttypes":7}]},{},[1]);
