64524935c818f99823772d10a0fb323055c2a7da
Lenin Luque <xleninx@gmail.com>
2015-09-07 18:56:19 -0300

    Add course for dynamics routes

--- router/index.js
+++ router/index.js
@@ -1,17 +1,29 @@
 const path = require('path')
+const course = require('course')
 const st = require('st')
+const router = course()
 
 const mount = st({
   path: path.join(__dirname, '..', 'public'),
-  index: 'index.html'
+  index: 'index.html',
+  passthrough: true
 })
 
 function onRequest(req, res){
   mount(req, res, function(err){
-    if (err) return res.end(err.message)
-    res.statusCode = 404
-    res.end(`Not found ${req.url}`)
+    if (err) return fail(err, res)
+    router(req, res, function(err){
+      if (err) return fail(err, res)
+      res.statusCode = 404
+      res.end(`Not found ${req.url}`)
+    })
+
   })
 }
 
+function fail(err, res){
+  res.statusCode = 500
+  res.setHeader('Content-type', 'text/plain')
+  res.end(err.message)
+}
 module.exports = onRequest
