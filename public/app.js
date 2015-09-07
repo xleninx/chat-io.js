a20f85e8e6834b313c15291adfd56ff1d4af282b
Lenin Luque <xleninx@gmail.com>
2015-09-07 14:25:51 -0300

    Serve app.js, add fixed routes

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const record = document.querySelector('#record')
+
+record.addEventListener('click', function(e){
+  e.preventDefault()
+
+  console.log('Button clicked')
+})
--- server.js
+++ server.js
@@ -16,10 +16,37 @@ server.listen(port)
 
 // Functions
 function onRequest(req, res){
+  let uri = req.url
+
+  if ( uri.startsWith('/index') || uri === '/' ){
+    return serveIndex(res)
+  }
+  if ( uri.startsWith('/app.js') ){
+    return serveApp(res)
+  }
+
+  res.statusCode = 404
+  res.end(`404 not found ${uri}`)
+}
+
+function serveIndex(res){
   let fileName = path.join(__dirname, 'public', 'index.html') // use path for compatibility with other OS
   let rs = fs.createReadStream(fileName)
 
   res.setHeader('Content-Type', 'text/html')
+  rs.pipe(res)
+
+  res.on('error', function(err){
+    res.setHeader('Content-Type', 'text/pain')
+    res.end(err.message)
+  })
+}
+
+function serveApp(res){
+  let app = path.join(__dirname, 'public', 'app.js') // use path for compatibility with other OS
+  let rs = fs.createReadStream(app)
+
+  res.setHeader('Content-Type', 'text/javascript')
   rs.pipe(res)
 
   res.on('error', function(err){
