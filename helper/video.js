ce85c371b1ed788fea7187e848fece2464f4ef7c
Lenin Luque <xleninx@gmail.com>
2015-09-07 20:02:13 -0300

    Decode images and save in hard disk

--- helper/video.js
+++ helper/video.js
@@ -1,9 +1,18 @@
 'use strict'
+
+const os = require('os')
 const async = require('async')
+const path = require('path')
+const fs = require('fs')
+const dataURIBuffer = require('data-uri-to-buffer')
+const uuid = require('uuid')
 const EventEmitter = require('events').EventEmitter
 
 module.exports = function (images){
   let events = new EventEmitter()
+  let count = 0
+  let baseName = uuid.v4()
+  let tmpDir = os.tmpDir()
 
   async.series([
     decodeImages,
@@ -13,7 +22,18 @@ module.exports = function (images){
   ], convertFinish)
 
   function decodeImages(done){
-    done()
+    async.eachSeries(images, decodeImage, done)
+  }
+
+  function decodeImage(image, done){
+    let fileName = `${baseName}-${count++}.jpg`
+    let buffer = dataURIBuffer(image)
+    let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+
+    ws.on('error', done)
+    .end(buffer, done)
+
+    events.emit('log', `Converting ${fileName}`)
   }
 
   function createVideo(done){
