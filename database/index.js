f65132aa775503376357b707d370c935ae0e90aa
Lenin Luque <xleninx@gmail.com>
2015-09-08 18:25:13 -0300

    Create records in database temporaly

--- database/index.js
+++ database/index.js
@@ -1,10 +1,22 @@
 'use strict'
+const uuid = require('uuid')
+const level = require('level')
+const ttl = require('level-ttl')
 
 module.exports = function(options){
   options = options || {}
+  let duration = optiones.duration || 10 * 60 * 1000
+
+  const db = ttl(level('./messages.db'), { checkFrequency: 10000 })
 
   function save(message, callback){
-    callback()
+    let key = `message-${Date.now()}-${uuid.v4()}`
+    let options = {
+      valueEncoding: 'json',
+      ttl: duration
+    }
+
+    db.put(key, message, options, callback)
   }
 
   function list(callback){
