25513eeef2b963cda80884cc8471f37669e9e525
Lenin Luque <xleninx@gmail.com>
2015-09-08 18:05:03 -0300

    add database module

--- realtime/index.js
+++ realtime/index.js
@@ -1,10 +1,11 @@
 'use strict'
 const helper = require('../helper')
 const socketio = require('socket.io')
+const database = require('./database')
 
 module.exports = function(server){
   const io = socketio(server)
-
+  const db = database()
   io.on('connection', onConnection)
 
   function onConnection(socket){
@@ -16,7 +17,7 @@ module.exports = function(server){
       converter.on('video', function(video){
         delete message.frames
         message.video = video
-
+        db.save(message, function(err){})
         socket.broadcast.emit('message', message)
         socket.emit('messageack', message)
       })
